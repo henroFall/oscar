5db621c4e2f4621ae97f490bdf77e163b7a316ce
henroFall <henryfell@gmail.com>
2020-06-15 22:04:38 -0500

    Update install_oscar.sh

--- install_oscar.sh
+++ install_oscar.sh
@@ -125,14 +125,14 @@ echo
 if [[ $(lsb_release -rs) == "20.04" ]]; then 
 
        echo "Ubuntu 20.04 detected, installing package: python2."
-       apt install -y python2
+       apt -y install python2
 	   check_exit_status
 else
        echo "$(lsb_release -rs) detected. Installing package: python."
-	   apt install -y python
+	   apt -y install python
 	   check_exit_status
 fi
-apt install -y sed curl git supervisor build-essential nodejs npm
+apt -y install sed curl git supervisor build-essential nodejs npm
 check_exit_status
 curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
 check_exit_status
